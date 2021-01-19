69968c4e2f9fe1b8cbe410789b00ed7f72112862
Chase杨念 <65386393+Little-YangNian@users.noreply.github.com>
2021-01-19 23:01:49 +0800

    add Python version
    It like Yun YouJun

---
+++ languages/Python/Girl.py
@@ -0,0 +1,8 @@
+class girl():
+    def __init__(self,name,boyname,hatename):
+        self.name = name
+        self.boyname = boyname
+        self.hatename = hatename
+    def say(self):
+        print(f'{self.name} 的男朋友是 {self.boyname} ,她讨厌 {self.hatename}')
+         
