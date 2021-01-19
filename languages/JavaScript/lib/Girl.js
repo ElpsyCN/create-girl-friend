d2990921b1575c547def62e0ccd5d3fb098ad000
qingzinai <2150495552@qq.com>
2021-01-19 15:57:50 +0800

    :memo:Blog:www.qingzinai.com :computer:  js版本Girl

--- languages/JavaScript/lib/Girl.js
+++ languages/JavaScript/lib/Girl.js
@@ -1,16 +1,12 @@
-const Girl=new Object()
-Girl.name="纸片人"
-Girl.boyFriend="我"
-Girl.say="爱"
-Girl.say2="厌恶"
-Girl.hate="牛头人"
-console.log(Girl.name+Girl.say+Girl.boyFriend)
-const laowang=new Object()
-laowang.name="牛头人"
-laowang.like="ntr"
-console.log(Girl.name+Girl.say+Girl.boyFriend+Girl.say2+laowang.name)
-const me=new Object()
-me.name="纯爱战士"
-me.occupation="纯爱战士"
-me.say="杀光牛头人"
-console.log(me.name+me.say)
+class Girl {
+  constructor(name, boyFriend, hateName) {
+    this.name = name;
+    this.boyFirend = boyFriend;
+    this.hateName = hateName
+  }
+  say() {
+    console.log(this.name + "的男朋友是" + this.boyFriend + "她讨厌" + this.hateName);
+  }
+};
+const girlFriend = new Parent('纸片人', "纯爱战士", "牛头人");
+girlFriend.say()
