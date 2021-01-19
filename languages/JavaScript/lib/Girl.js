427c6bccdceee5cc99daa8054880120ed52f59c4
qingzinai <2150495552@qq.com>
2021-01-19 15:24:01 +0800

    js

---
+++ languages/JavaScript/lib/Girl.js
@@ -0,0 +1,16 @@
+const Girl=new Object()
+Girl.name="纸片人"
+Girl.boyFriend="我"
+Girl.say="爱"
+Girl.say2="厌恶"
+Girl.hate="牛头人"
+console.log(Girl.name+Girl.say+Girl.boyFriend)
+const laowang=new Object()
+laowang.name="牛头人"
+laowang.like="ntr"
+console.log(Girl.name+Girl.say+Girl.boyFriend+Girl.say2+laowang.name)
+const me=new Object()
+me.name="纯爱战士"
+me.occupation="纯爱战士"
+me.say="杀光牛头人"
+console.log(me.name+me.say)
