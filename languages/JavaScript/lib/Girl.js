de8bacdbc48ea0bb3bfe8bdbc4bbabeb7eef0ff2
qingzinai <2150495552@qq.com>
2021-01-19 17:31:42 +0800

    :busts_in_silhouette: Add JavaScript version

--- languages/JavaScript/lib/Girl.js
+++ languages/JavaScript/lib/Girl.js
@@ -1,12 +1,13 @@
-class Girl {
+export default class Girl {
+
   constructor(name, boyFriend, hateName) {
     this.name = name;
     this.boyFirend = boyFriend;
     this.hateName = hateName
   }
+
   say() {
     console.log(this.name + "的男朋友是" + this.boyFriend + "她讨厌" + this.hateName);
   }
+
 };
-const girlFriend = new Girl('纸片人', "纯爱战士", "牛头人");
-girlFriend.say()
---
+++ languages/JavaScript/lib/Me.js
@@ -0,0 +1,6 @@
+import GirlFriend from './Girl.js'
+
+const GirlFriend = new GirlFriend("纯爱战士", "纸片人", "牛头人")
+
+GirlFriend.say()
+
