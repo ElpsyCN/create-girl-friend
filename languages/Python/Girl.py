a0822af8db596a3a05981855ceb273d2b40192aa
Chase杨念 <65386393+Little-YangNian@users.noreply.github.com>
2021-01-20 23:40:14 +0800

    📝Add New Fuction (#14)
    * 📝Add New Fuction
    
    * Update README.md

--- languages/Python/Girl.py
+++ languages/Python/Girl.py
@@ -1,8 +1,20 @@
+import requests
+import json
 class girl():
     def __init__(self,name,boyname,hatename):
         self.name = name
         self.boyname = boyname
         self.hatename = hatename
     def say(self):
-        print(f'{self.name} 的男朋友是 {self.boyname} ,她讨厌 {self.hatename}')
-         
+        print(f'我 {self.name} 的男朋友是 {self.boyname} ,我讨厌 {self.hatename}')
+    def say_nb(self):
+        respoen = requests.get('https://el-bot-api.vercel.app/api/words/niubi')
+        # 熟悉的api
+        get_list = json.loads(respoen.text)
+        rest = str(get_list[0])
+        print(rest.replace('${name}',self.boyname))
+    def say_wanan(self):
+        respoen = requests.get('https://el-bot-api.vercel.app/api/words/wanan')
+        # en......
+        rest = json.loads(respoen.text)
+        print(rest[0])
